44f23e12102bcf258679bdc9d0790cf4bc9974f4
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-14 14:06:53 +0530

    Update myown.py

--- tobrot/plugins/myown.py
+++ tobrot/plugins/myown.py
@@ -25,13 +25,13 @@ from tobrot.helper_funcs.help_Nekmo_ffmpeg import mux_video , mux_do_video
 async def mux_f(client, message):
     status_message = await message.reply_text("Processing ...")
     a , b , c= message.text.split("|")
-    await status_message.edit(a b c)
+    await status_message.edit(c)
     location = await mux_video(a,b,c)
     await status_message.edit(location)
     
 async def mux_do_f(client, message):
     status_message = await message.reply_text("Processing ...")
     a , b, c= message.text.split("|")
-    await status_message.edit(a b c)
+    await status_message.edit(a)
     location = await mux_do_video(a,b,c)
     await status_message.edit(location)
