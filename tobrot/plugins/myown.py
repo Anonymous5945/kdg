706d9cba13849a22e99196ecd48c1433df534586
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-15 13:12:44 +0530

    Update myown.py

--- tobrot/plugins/myown.py
+++ tobrot/plugins/myown.py
@@ -38,10 +38,10 @@ async def mux_f(client, message):
     
 async def mux_do_f(client, message):
     status_message = await message.reply_text("Processing ...")
-    a , b , c= message.text.split("|")
+    a , b, c= message.text.split("|")
     file = a.split(" ")[1]
     sub = b.split(" ")[1]
-    out = c.split(" ")[1]
+    output = c.split(" ")[1]
     mcover="/app/Docover.jpg"
     await run_command(["ffmpeg", "-i", file, "-i", sub, "-c", "copy", "-attach", mcover, "-metadata:s:t", "mimetype=image/jpeg", "-metadata:s:t", "filename=cover.jpg", "-metadata", "title=Upl'd By Team-D&O @dramaost TG Group", output])
     await status_message.edit(output)
@@ -49,10 +49,10 @@ async def mux_do_f(client, message):
  
 async def remux_f(client, message):
     status_message = await message.reply_text("Processing ...")
-    a , b ,c= message.text.split("|")
+    a , c= message.text.split("|")
     file = a.split(" ")[1]
-    mcover = b.split(" ")[1]
-    out = c.split(" ")[1]
-    await run_command(["ffmpeg", "-i", file, "-c", "copy", "-attach", mcover, "-metadata:s:t", "mimetype=image/jpeg", "-metadata:s:t", "filename=cover.jpg", "-metadata", "title=Upl'd By Team-D&O @dramaost TG Group", output])
+    output = c.split(" ")[1]
+    mcover="/app/Docover.jpg"
+    await run_command(["ffmpeg", "-i", file, "-c", "copy", "-attach", mcover, "-metadata:s:t", "mimetype=image/jpeg", "-metadata:s:t", "filename=cover.jpg", "-metadata", "title=Upl'd & Enc'd By Team-D&O @dramaost TG Group", output])
     await status_message.edit(output)
    
