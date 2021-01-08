ab7205a2cb250cc5843c47e332eb443045fe75bd
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2021-01-08 20:00:15 +0530

    Update link_fliter.py

--- tobrot/helper_funcs/link_fliter.py
+++ tobrot/helper_funcs/link_fliter.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
 from pyrogram import filters
 
 
