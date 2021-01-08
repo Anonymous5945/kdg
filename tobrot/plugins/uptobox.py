21069bfd1ddaf03b1a906dd3bcbab7b275d8579e
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2021-01-08 19:59:40 +0530

    Update uptobox.py

--- tobrot/plugins/uptobox.py
+++ tobrot/plugins/uptobox.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
 import json
 import os
 import shutil
 import time
 from pyrogram import filters
 from tobrot import (
-    LOGGER,
     AUTH_USERS,
     UPTO_BOX_TOKEN,
     TG_MAX_FILE_SIZE,
