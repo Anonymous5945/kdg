ee562bf5ba07151c8ed5d1b2a3fc1ab38695ebdc
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2021-03-04 20:54:24 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -28,16 +28,10 @@ from tobrot import (
     Clear_thumb_CMD_TRIGGER,
     TELEGRAM_CMD_TRIGGER,
     Mass_CMD_TRIGGER,
-    Scrapx_CMD_TRIGGER,
-    Remux_CMD_TRIGGER,
-    Multi_CMD_TRIGGER,
-    Arch_CMD_TRIGGER,
     Youmux_CMD_TRIGGER,
     Getmux_CMD_TRIGGER,
     Gd_CMD_TRIGGER,
     Gpd_CMD_TRIGGER,
-    Wetv_CMD_TRIGGER,
-    Vid_CMD_TRIGGER,
     kdg1_CMD_TRIGGER,
     kdg2_CMD_TRIGGER
 )
@@ -50,9 +44,7 @@ from pyrogram.handlers import (
     MessageHandler,
     CallbackQueryHandler
 )
-from tobrot.plugins.uptobox import leech_upto_box_fn
-from tobrot.plugins.ffmown import multi_f, arch_f
-from tobrot.plugins.myown import mux_f, domux_f, remux_f, muxget_f, muxyou_f, gdfile_f, vid_f
+from tobrot.plugins.myown import muxget_f, muxyou_f, gdfile_f
 from tobrot.plugins.new_join_fn import new_join_f, help_message_f
 from tobrot.plugins.incoming_message_fn import incoming_youtube_dl_f
 from tobrot.plugins.status_message_fn import (
@@ -66,8 +58,7 @@ from tobrot.plugins.custom_thumbnail import (
     clear_thumb_nail
 )
 from tobrot.plugins.forwardg import kdg1_f,kdg2_f
-from tobrot.helper_funcs.download import down_load_media_f, mass_down_load_media_f , scrap_seg_media_f, gp_f , wetv_f
-from tobrot.helper_funcs.link_fliter import _link_match_filt_er
+from tobrot.helper_funcs.download import down_load_media_f, mass_down_load_media_f
 
 
 if __name__ == "__main__" :
@@ -84,24 +75,6 @@ if __name__ == "__main__" :
         workdir=DOWNLOAD_LOCATION
     )
     #
-    incoming_vid_handler = MessageHandler(
-        vid_f,
-        filters=filters.command([Vid_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_vid_handler)
-    #
-    incoming_gpd_handler = MessageHandler(
-        gp_f,
-        filters=filters.command([Gpd_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_gpd_handler)
-    #
-    incoming_wetv_handler = MessageHandler(
-        wetv_f,
-        filters=filters.command([Wetv_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_wetv_handler)
-    #
     incoming_youtube_dl_handler = MessageHandler(
         incoming_youtube_dl_f,
         filters=filters.command([Ytdl_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
@@ -113,12 +86,6 @@ if __name__ == "__main__" :
         filters=filters.command([Mass_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_mass_download_handler)
-    #
-    incoming_scrap_handler = MessageHandler(
-        scrap_seg_media_f,
-        filters=filters.command([Scrapx_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_scrap_handler)
     #
     incoming_telegram_download_handler = MessageHandler(
         down_load_media_f,
@@ -137,24 +104,6 @@ if __name__ == "__main__" :
         filters=filters.command([kdg2_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_kdg2_handler)
-    #
-    incoming_remux_handler = MessageHandler(
-        remux_f,
-        filters=filters.command([Remux_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_remux_handler)
-    #
-    incoming_multi_handler = MessageHandler(
-        multi_f,
-        filters=filters.command([Multi_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_multi_handler)
-    #
-    incoming_arch_handler = MessageHandler(
-        arch_f,
-        filters=filters.command([Arch_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_arch_handler)
     #
     incoming_muxyou_handler = MessageHandler(
         muxyou_f,
@@ -220,11 +169,5 @@ if __name__ == "__main__" :
         filters=filters.command([Clear_thumb_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(clear_thumb_nail_handler)
-    #
-    uptobox_handler = MessageHandler(
-        leech_upto_box_fn,
-        filters=filters.chat(chats=AUTH_CHANNEL) & _link_match_filt_er("uptobox.com")
-    )
-    app.add_handler(uptobox_handler)
     #
     app.run()
