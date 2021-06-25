311936fd90145c8ee332f71d46f87ed3da3f7d0a
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2021-06-25 17:36:11 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -21,11 +21,7 @@ from tobrot import (
     API_HASH,
     AUTH_CHANNEL,
     EXEC_CMD_TRIGGER,
-    Eval_CMD_TRIGGER,
-    Upload_CMD_TRIGGER,
-    Save_Thumb_CMD_TRIGGER,
-    Clear_thumb_CMD_TRIGGER,
-    TELEGRAM_CMD_TRIGGER
+    Eval_CMD_TRIGGER
 )
 
 from pyrogram import (
@@ -36,18 +32,11 @@ from pyrogram.handlers import (
     MessageHandler,
     CallbackQueryHandler
 )
-from tobrot.plugins.new_join_fn import new_join_f, help_message_f
+
 from tobrot.plugins.status_message_fn import (
     exec_message_f,
-    eval_message_f,
-    upload_document_f
+    eval_message_f
 )
-from tobrot.plugins.custom_thumbnail import (
-    save_thumb_nail,
-    clear_thumb_nail
-)
-from tobrot.helper_funcs.download import down_load_media_f
-
 
 if __name__ == "__main__" :
     # create download directory, if not exist
@@ -62,13 +51,6 @@ if __name__ == "__main__" :
         workers=343,
         workdir=DOWNLOAD_LOCATION
     )
-    
-    #
-    incoming_telegram_download_handler = MessageHandler(
-        down_load_media_f,
-        filters=filters.command([TELEGRAM_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(incoming_telegram_download_handler)
     #
     exec_message_handler = MessageHandler(
         exec_message_f,
@@ -81,35 +63,5 @@ if __name__ == "__main__" :
         filters=filters.command([Eval_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(eval_message_handler)
-    #
-    upload_document_handler = MessageHandler(
-        upload_document_f,
-        filters=filters.command([Upload_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(upload_document_handler)
-
-    help_text_handler = MessageHandler(
-        help_message_f,
-        filters=filters.command(["help"]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(help_text_handler)
-    #
-    new_join_handler = MessageHandler(
-        new_join_f,
-        filters=~filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(new_join_handler)
-    #
-    save_thumb_nail_handler = MessageHandler(
-        save_thumb_nail,
-        filters=filters.command([Save_Thumb_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(save_thumb_nail_handler)
-    #
-    clear_thumb_nail_handler = MessageHandler(
-        clear_thumb_nail,
-        filters=filters.command([Clear_thumb_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(clear_thumb_nail_handler)
     #
     app.run()
